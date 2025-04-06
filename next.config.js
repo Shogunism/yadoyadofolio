6ee1f370488913fbb9c14ff8937dc61245a484fd
Shogunism <k23067kk@aitech.ac.jp>
2025-04-06 23:17:51 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,20 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
   images: {
-    remotePatterns: [
-      {
-        protocol: "http",
-        hostname: "localhost", // 開発環境用
-        port: "3000", // ローカルサーバーのポート
-        pathname: "/**", // 任意のパスを許可
-      },
-      {
-        protocol: "https",
-        hostname: "yadoyadoportfolio.vercel.app", // 本番環境用
-        pathname: "/**", // 任意のパスを許可
-      },
-    ],
+    domains: ["yadoyadofolio-otameshi.vercel.app"], // 外部画像のホスト名を追加
   },
 };
 
