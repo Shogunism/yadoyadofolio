6898770805d765ff53b321ef690a5930319e2627
Shogunism <k23067kk@aitech.ac.jp>
2025-04-07 23:11:38 +0900

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,9 +5,70 @@ import Link from 'next/link';
 import Nav from '../components/Nav'; // Navコンポーネントをインポート
 import '../styles/globals.css';
 
+// メタデータ定義
+export const metadata = {
+  title: "やどやど - Minecraft豊橋再現　ギャラリー",
+  description: "Minecraftで豊橋を再現したプロジェクトのギャラリー",
+  icons: {
+    icon: "/favicon.ico",
+  },
+  openGraph: {
+    title: "やどやど - Minecraft豊橋再現 ギャラリー",
+    description: "Minecraftで豊橋を再現したプロジェクトのギャラリー",
+    url: "https://yadoyadoportfolio.vercel.app/",
+    type: "website",
+    images: [
+      {
+        url: "https://yadoyadoportfolio.vercel.app/thumbnail.png",
+        width: 1200,
+        height: 630,
+        alt: "Minecraft豊橋再現 Thumbnail",
+      },
+    ],
+    siteName: "やどやど - Minecraft豊橋再現 ギャラリー",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "やどやど - Minecraft豊橋再現 ギャラリー",
+    description: "Minecraftで豊橋を再現するプロジェクトのギャラリー",
+    images: ["https://yadoyadoportfolio.vercel.app/thumbnail.png"],
+  },
+};
+
+export const viewport = {
+  width: "device-width",
+  initialScale: 1.0,
+};
+
+
+
 const Layout = ({ children }: { children: ReactNode }) => {
   return (
     <html lang="ja">
+      <head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content={viewport.width} />
+        <meta name="description" content={metadata.description} />
+        <link rel="icon" href={metadata.icons.icon} />
+        
+        {/* Open Graph Meta Tags */}
+        <meta property="og:title" content={metadata.openGraph.title} />
+        <meta property="og:description" content={metadata.openGraph.description} />
+        <meta property="og:url" content={metadata.openGraph.url} />
+        <meta property="og:type" content={metadata.openGraph.type} />
+        <meta property="og:image" content={metadata.openGraph.images[0].url} />
+        <meta property="og:image:width" content={metadata.openGraph.images[0].width.toString()} />
+        <meta property="og:image:height" content={metadata.openGraph.images[0].height.toString()} />
+        <meta property="og:image:alt" content={metadata.openGraph.images[0].alt} />
+        <meta property="og:site_name" content={metadata.openGraph.siteName} />
+
+        {/* Twitter Meta Tags */}
+        <meta name="twitter:card" content={metadata.twitter.card} />
+        <meta name="twitter:title" content={metadata.twitter.title} />
+        <meta name="twitter:description" content={metadata.twitter.description} />
+        <meta name="twitter:image" content={metadata.twitter.images[0]} />
+        <title>{metadata.title}</title>
+      </head>
       <body style={{ paddingTop: '103px' }}>
         <div className="container">
           <Nav /> {/* Navコンポーネントをここにぶちこむ */}
