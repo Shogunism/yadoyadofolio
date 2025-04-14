03919e6dcf0859cb80f82dbb921efdaedd1b535c
Shogunism <k23067kk@aitech.ac.jp>
2025-04-14 11:51:27 +0900

    added_fool_tool

---
+++ app/poyo/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import React, { useEffect } from 'react';
+
+const PoyoPage = () => {
+  useEffect(() => {
+    window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+  }, []);
+
+  return null; 
+};
+
+export default PoyoPage;
