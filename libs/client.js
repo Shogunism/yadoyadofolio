62dd7efeefe24a34f1aed32f8ffd9dcfa6b97473
Shogunism <k23067kk@aitech.ac.jp>
2025-05-20 13:52:42 +0900

    iroiro2

---
+++ libs/client.js
@@ -0,0 +1,6 @@
+import {createClient, createClinet} from "microcms-js-sdk"; 
+
+export const client = createClient({
+    serviceDomain : "yadoyado",
+    apiKey : process.env.MICROCMS_API_KEY,
+});
--- next.config.js
+++ next.config.js
@@ -1,9 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["yadoyado.me"], // 外部画像のホスト名を追加
+    domains: ['images.microcms-assets.io'], // ←これを追加
   },
 };
 
-//value!!!!
 module.exports = nextConfig;
