8135ff50c741452e94b92a69ad8a490d5ee24fc5
Shogunism <k23067kk@aitech.ac.jp>
2026-04-16 12:15:02 +0900

    ｗ

--- next.config.js
+++ next.config.js
@@ -1,8 +1,23 @@
 // next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
   images: {
-    domains: ['images.microcms-assets.io'], // ← 必要な外部ドメイン
+    // Deprecated `domains` replaced by `remotePatterns` to avoid build warning
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.microcms-assets.io',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: '**.microcms-assets.io',
+        port: '',
+        pathname: '/**',
+      },
+    ],
   },
   env: {
     MICROCMS_SERVICE_DOMAIN: process.env.MICROCMS_SERVICE_DOMAIN,
